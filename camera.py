38b0a418ded616900506129a7101c66c1ea31a36
Georg1986 <georg.auer@gmail.com>
2021-02-27 11:03:24 +0100

    win and raspi compatible

--- camera.py
+++ camera.py
@@ -3,19 +3,29 @@
 #Desc: This scrtipt script..
 
 import cv2
-from imutils.video.pivideostream import PiVideoStream
+try:
+    from imutils.video.pivideostream import PiVideoStream
+except:
+    print("No Raspberry/No Raspberry Cam found")
 import imutils
 import time
 import numpy as np
 
 class VideoCamera(object):
     def __init__(self, flip = False):
-        self.vs = PiVideoStream().start()
+        try:
+            self.vs = PiVideoStream().start()
+        except:
+            self.vs = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+            #0 is the standard number of the connected camera in windows
         self.flip = flip
         time.sleep(2.0)
 
     def __del__(self):
-        self.vs.stop()
+        try:
+            self.vs.stop()
+        except:
+            self.vs.release()
 
     def flip_if_needed(self, frame):
         if self.flip:
@@ -23,6 +33,12 @@ class VideoCamera(object):
         return frame
 
     def get_frame(self):
-        frame = self.flip_if_needed(self.vs.read())
-        ret, jpeg = cv2.imencode('.jpg', frame)
+        try:
+            frame = self.flip_if_needed(self.vs.read())
+            ret, jpeg = cv2.imencode('.jpg', frame)
+        except:
+            ret, frame = self.vs.read()
+            ret, jpeg = cv2.imencode('.jpg', frame)
+
+        #ret, jpeg = cv2.imencode('.jpg', frame)
         return jpeg.tobytes()
